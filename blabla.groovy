6999605cf1255ad032c3b6530b622922c6de1120
shruxx <eisdrachen@gmail.com>
2018-08-30 12:13:59 +0200

    spielzeug

--- blabla.groovy
+++ blabla.groovy
@@ -1,2 +1,24 @@
+import groovy.swing.SwingBuilder
+import javax.swing.*
+import java.awt.*
+import javax.swing.WindowConstants as WC
 
-erthsghydhdsghsdhjs
+def eingabefeld = new JTextField()
+def gui = new SwingBuilder()
+
+gui.edt {
+    frame(title: 'Einkaufsliste', defaultCloseOperation: JFrame.EXIT_ON_CLOSE, pack: true, show: true)
+            {
+                vbox {
+                    textlabel = label('Was benötigst du?')
+                    hbox{
+                        input = textField(columns:20, actionPerformed: {echo.text = input.text.toUpperCase()})
+                        //HIER MUSS NOCH GANZ VIEL STEHEN
+
+                     hbox{
+                         button(text:'Mitteilen', actionPerformed: {println input.text})
+                     }
+                    }
+                }
+            }
+}
