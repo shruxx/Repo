5b3dfcd41e714ec180a7fd6181424f66fe4db71b
shruxx <eisdrachen@gmail.com>
2018-08-31 11:10:18 +0200

    spielzeug

--- blabla.groovy
+++ blabla.groovy
@@ -1,15 +1,17 @@
 import groovy.sql.Sql
 import groovy.swing.SwingBuilder
+import groovyx.gpars.dataflow.Select
+
 import javax.swing.*
 import java.awt.*
 import javax.swing.WindowConstants as WC
 
-def url = 'jdbc:sqlserver://vm-srv-sql1:1433;databaseName=TestDBDev'
-def user = 'arge_webintranet'
+def db = 'jdbc:sqlserver://vm-srv-sql1:1433;databaseName=TestDBDev'
+def user = 'arge_dev'
 def password = 'arge'
 def eingabefeld = new JTextField()
 def driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-def sql = Sql.newInstance(url, user, password, driver)
+def sql = Sql.newInstance(db, user, password, driver)
 def gui = new SwingBuilder()
 
 gui.edt {
@@ -18,16 +20,22 @@ gui.edt {
                 vbox {
                     textlabel = label('Was benötigst du?')
                     hbox {
-                        input = textField(columns: 20, actionPerformed: { echo.text = input.text.toUpperCase() })
+                        input = textField(columns: 50, actionPerformed: { echo.text = input.text.toUpperCase()})
+
                         //HIER MUSS NOCH GANZ VIEL STEHEN
+                    }
+                    hbox {
+                        button(text: 'zeige mir die List an', actionPerformed: {
+                            (sql.eachRow("SELECT * FROM Einkaufsliste") {
+                               println "${it.Ware}"
+                            })
+                        })
 
                         hbox {
-                            button(text: 'Mitteilen', actionPerformed: {(sql.eachRow("SELECT * FROM ARGE_AD_Gruppen") {
-                                println "${it.Group_ID} | ${it.Group_Name}"
-                                //println(input.text);
-                                })
-                            }
-                            )
+                            button(text: 'Mitteilen', actionPerformed: {
+                                sql.executeInsert"""INSERT INTO Einkaufsliste (Ware) VALUES (${input.text})"""
+                                println "${input.text} wurde vermerkt!"
+                            })
                         }
                     }
                 }
