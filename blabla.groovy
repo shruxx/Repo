148c92bf92986b2d50297d9db36ab1a521946d13
shruxx <eisdrachen@gmail.com>
2018-09-04 10:50:19 +0200

    spielzeug

--- blabla.groovy
+++ blabla.groovy
@@ -5,6 +5,8 @@ import groovyx.gpars.dataflow.Select
 import javax.swing.*
 import java.awt.*
 import javax.swing.WindowConstants as WC
+import java.awt.event.ActionEvent
+import java.text.DecimalFormat
 
 def db = 'jdbc:sqlserver://vm-srv-sql1:1433;databaseName=TestDBDev'
 def user = 'arge_dev'
@@ -26,7 +28,7 @@ gui.edt {
                         //HIER MUSS NOCH GANZ VIEL STEHEN
                     }
                     hbox {
-                        button(text: 'zeige mir die List an', actionPerformed: {
+                        button(text: 'zeige mir die Liste an', actionPerformed: {
                             (sql.eachRow("SELECT * FROM Einkaufsliste") {
                                 println "${it.Ware}"
                             })
@@ -35,13 +37,44 @@ gui.edt {
                         hbox {
                             button(text: 'Mitteilen', actionPerformed: {
                                 println input.text.length()
-                                if (input.text.length() < 50){println "toll!"} else { throw new Exception("Zu viele Zeichen eingegeben!")}
-                                sql.executeInsert"""INSERT INTO Einkaufsliste (Ware) VALUES (${input.text})"""
+                                if (input.text.length() < 50){println "toll!"}
+                                else { throw new Exception("Zu viele Zeichen eingegeben!")}
+                                if(input.text.length() > 1) {println "du musst was eingeben!"}
+                                else { throw new Exception("Zu wenig Zeichen angegeben!")}
+                                //sql.executeInsert"""INSERT INTO Einkaufsliste (Ware) VALUES (${input.text})"""
                                 println "${input.text} wurde vermerkt!"
-                            })
+                                frame(title: 'Preis', defaultCloseOperation: JFrame.DISPOSE_ON_CLOSE, pack: true, show: true){
+                                    hbox {
+                                        inputP = textField(columns: 30, actionPerformed: { echo.text = inputP.text.toUpperCase()})
+                                            textlabelP = label("Was kostet ${input.text}?")
+                                                    button(text:"Mitteilen", actionPerformed: {
+                                                        if(input.text.length() > 1) {println "Cool"}
+                                                        else { throw new Exception("Zu wenig Zeichen angegeben!")}
+                                                        //sql.executeInsert"""INSERT INTO Einkaufsliste (Preis, Ware) VALUES (${inputP.text},${input.text})"""
+                                                        println "${inputP.text} wurde vermerkt"
+                                                        frame(title: 'Menge', defaultCloseOperation: JFrame.EXIT_ON_CLOSE, pack: true, show: true){
+                                                            hbox {
+                                                                inputM = textField(columns: 30, actionPerformed: { echo.text = inputM.text.toUpperCase()})
+                                                                textlabelM = label ("Wieviel hättest du gerne von (${input.text})?")
+                                                                button(text:"Mitteilen", actionPerformed: {
+                                                                    if(inputM.text.length() > 1) {println "fast fertig!"}
+                                                                    else { throw new Exception("Zu wenig Zeichen angegeben!")}
+                                                                    String m = "${inputM.text}"
+                                                                    def z = m as Double
+                                                                    sql.executeInsert"""INSERT INTO Einkaufsliste (Menge, Preis, Ware) VALUES(${z}, ${inputP.text}, ${input.text})"""
+                                                                    println "${inputM.text} wurde vermerkt"
+                                                                                                                                    })
+                                                                button(text:"Schliessen", actionPerformed: {gui.dispose()})
+                                                                }
+                                                            }
+                                                        })
+                                                    }
+                                        }
+                                })
+                            }
                             hbox{
                                 button(text: 'Einkaufsliste resetten', actionPerformed: {
-                                    frame(title: 'Einkaufsliste', defaultCloseOperation: JFrame.EXIT_ON_CLOSE, pack: true, show: true){
+                                    frame(title: 'Einkaufsliste', defaultCloseOperation: JFrame.DISPOSE_ON_CLOSE, pack: true, show: true){
                                         hbox {
                                             textlabel = label ('Bist du dir wirklich sicher?')
                                             button(text:'Aber sicher doch', actionPerformed: {
@@ -57,4 +90,3 @@ gui.edt {
                     }
                 }
             }
-}
